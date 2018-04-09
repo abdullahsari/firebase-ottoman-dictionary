467e4f2fac9acdb454c804eb28bea9c4a6b28cfd
Muhammed Sari <muhammed.abdullah.sari@hotmail.com>
2018-04-09 23:35:48 +0200

    Added onMeaningCreate function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,14 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+export const onMeaningCreate = functions
+    .database
+    .ref('glossaries/{uid}/meanings/{word}')
+    .onCreate(async (s, e) => {
+        const {uid, word} = e.params;
+        const char = word.charAt(0);
+        await admin.database().ref(`glossaries/${uid}/letters/${char}`).set(true);
+        await admin.database().ref(`glossaries/${uid}/words/${char}/${word}`).set(true);
+    });
