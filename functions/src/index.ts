bf5ccdc60178316a044baa90f190e59f4257bb93
Muhammed Sari <muhammed.abdullah.sari@hotmail.com>
2018-04-12 19:23:49 +0200

    add onMeaningDelete function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,12 +3,23 @@ import * as admin from 'firebase-admin';
 
 admin.initializeApp(functions.config().firebase);
 
+const meaning = 'glossaries/{uid}/meanings/{word}';
+
 export const onMeaningCreate = functions
     .database
-    .ref('glossaries/{uid}/meanings/{word}')
+    .ref(meaning)
     .onCreate(async (s, e) => {
         const {uid, word} = e.params;
         const char = word.charAt(0);
         await admin.database().ref(`glossaries/${uid}/letters/${char}`).set(true);
         await admin.database().ref(`glossaries/${uid}/words/${char}/${word}`).set(true);
+    });
+
+export const onMeaningDelete = functions
+    .database
+    .ref(meaning)
+    .onDelete((s, e) => {
+        const {uid, word} = e.params;
+        const char = word.charAt(0);
+        return admin.database().ref(`glossaries/${uid}/words/${char}/${word}`).remove();
     });
